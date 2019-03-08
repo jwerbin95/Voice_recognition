494366039e7c498cabb6afa838a01f61cf0b3cad
Jacob Werbin <jwerbin95@gmail.com>
2019-03-08 16:29:47 -0600

    added visualizer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AudioAnalyser from './AudioAnalyser';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      audio: null
+    };
+    this.toggleMicrophone = this.toggleMicrophone.bind(this);
+  }
+
+  async getMicrophone() {
+    const audio = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: false
+    });
+    this.setState({ audio });
+  }
+
+  stopMicrophone() {
+    this.state.audio.getTracks().forEach(track => track.stop());
+    this.setState({ audio: null });
+  }
+
+  toggleMicrophone() {
+    if (this.state.audio) {
+      this.stopMicrophone();
+    } else {
+      this.getMicrophone();
+    }
+  }
+  componentDidMount(){
+    this.getMicrophone();
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="controls">
+          <button onClick={this.toggleMicrophone}>
+            {this.state.audio ? 'Stop microphone' : 'Get microphone input'}
+          </button>
+        </div>
+        {this.state.audio ? <AudioAnalyser audio={this.state.audio} /> : ''}
       </div>
     );
   }
---
+++ src/AudioVisualiser.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+class AudioVisualiser extends Component {
+  constructor(props) {
+    super(props);
+    this.canvas = React.createRef();
+  }
+
+  componentDidUpdate() {
+    this.draw();
+  }
+
+    draw() {
+    const { audioData } = this.props;
+    const canvas = this.canvas.current;
+    const height = canvas.height;
+    const width = canvas.width;
+    const context = canvas.getContext('2d');
+    let x = 0;
+    const sliceWidth = (width * 1.0) / audioData.length;
+
+    context.lineWidth = 7;
+    context.clearRect(0, 0, width, height);
+
+    
+    for (const item of audioData) {
+      const y = (item / 255.0) * 400;
+      context.beginPath();
+      context.strokeStyle=`rgb(0, ${y+40}, ${x+100})`
+      context.arc(width/2, height/2, y+40, 0, Math.PI * 2, true)
+      context.stroke()
+      x += sliceWidth;
+    }
+    context.stroke();
+  }
+
+
+  render() {
+    return (
+      <div class="container">
+        <canvas width="1000" height="1000" ref={this.canvas} />;
+      </div>
+    )
+  }
+}
+
+export default AudioVisualiser;
