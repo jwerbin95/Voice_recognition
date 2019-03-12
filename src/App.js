2f40b372537a7ed672208894e369b2e896106a27
Jacob Werbin <jwerbin95@gmail.com>
2019-03-12 15:20:11 -0500

    Added several commands to voice recognition

--- src/App.js
+++ src/App.js
@@ -7,16 +7,22 @@ class App extends Component {
     super(props);
     this.state = {
       audio: null,
-      message: ''
+      message: '',
+      showForm: false,
+      command: "",
+      url: ""
     };
     this.toggleMicrophone = this.toggleMicrophone.bind(this);
     this.setMessage = this.setMessage.bind(this)
+    this.checkForm = this.checkForm.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   async getMicrophone() {
     const audio = await navigator.mediaDevices.getUserMedia({
       audio: true,
-      video: false
+      video: false,
     });
     this.setState({ audio });
   }
@@ -39,22 +45,63 @@ class App extends Component {
       message: newMessage
     })
   }
+
+  checkForm(){
+    this.setState({
+      showForm: true
+    })
+  }
   
   show(){
     return(
       <div>
         <AudioAnalyser audio={this.state.audio} />
-        <Dictaphone toggleMicrophone={this.toggleMicrophone} setMessage={this.setMessage}/>
+        <Dictaphone toggleMicrophone={this.toggleMicrophone} setMessage={this.setMessage} checkForm={this.checkForm}/>
       </div>
     )
+  }
+  handleSubmit(event){
+    event.preventDefault()
+    fetch("http://localhost:3000/command", {
+      method: 'post',
+      headers: {'Content-Type':'application/json'},
+      body: JSON.stringify({
+        name: this.state.command,
+        value: this.state.url
+      })
+    }).then(data=>{
+      console.log(data)
+      console.log("Sucessfully send post request to API.")
+    }).catch(error=>{
+      console.log(error.stack)
+    })
+  }
+  handleChange(event){
+    event.preventDefault()
+    this.setState({
+      [event.target.name]: event.target.value
+    })
   }
   render() {
     return (
       <div className="App">
-        <div class='transcriptContainer'>
+        <div className='transcriptContainer'>
           <span className="transcript">{this.state.message}</span>
         </div>
-        {this.state.audio ? this.show() : <img className="mic" src="images/microphone.png" onClick={this.toggleMicrophone} />}
+        {this.state.audio ? this.show() : <img className="mic" src="images/microphone.png" onClick={this.toggleMicrophone} alt="click to use voice commands"/>}
+        {this.state.showForm ? (
+          <form onSubmit={this.handleSubmit} className="ui form">
+            <div className="field">
+              <span className="formLabel">Command Name:</span>
+              <input className="inputField" type="text" name="command" placeholder="Take Me To Google..." value={this.state.command} onChange={this.handleChange}/>
+            </div>
+            <div className="field">
+              <span className="formLabel" name="url">URL:</span>
+              <input className="inputField" type="text" name="url" placeholder="https://www.google.com/..." value={this.state.url} onChange={this.handleChange}/>
+            </div>
+            <input className="ui button" type="submit" value="Save" />
+          </form>
+        ) : null}
       </div>
     );
   }
--- src/Dictaphone.js
+++ src/Dictaphone.js
@@ -12,37 +12,56 @@ const propTypes = {
 class Dictaphone extends Component {
   testTranscript(){
       console.log(this.props.finalTranscript)
-      fetch("http://localhost:3000/command/"+this.props.finalTranscript.toLowerCase()).then(data=>{
-        this.props.resetTranscript()
-        data.json().then(jsonData=>{
-          window.location.assign(jsonData[0].value)
+      let testScript = this.props.finalTranscript.split(" ")
+      if(testScript[0].toLowerCase()==="google"){
+        testScript.shift()
+        let urlString = ""
+        testScript.map(function(item){
+          urlString+=item+'+'
+          return item
         })
-          .catch(error=>{
-            this.props.toggleMicrophone()
-            this.props.setMessage("Command not found.")
-            setTimeout(function(){
-              this.props.setMessage("")
-            }.bind(this), 3000)
-          })
-        
-      }).catch(error=>{
-        console.log(error.stack)
+        urlString.slice(0, -1)
+        window.location.assign(`https://www.google.com/search?ei=EcaHXIXpF8PKswXV8JiQDQ&q=${urlString}&oq=${urlString}&gs_l=psy-ab.3..0l3j0i30l5j0i5i30j0i8i10i30.18609.18983..19185...0.0..0.171.494.2j2......0....1..gws-wiz.......0i71j0i7i30j0i8i7i10i30.L95Y-uyZKpM`)
+      }else if(this.props.finalTranscript.toLowerCase()==='add website' ){
         this.props.resetTranscript()
-      })
+        this.props.toggleMicrophone()
+        this.props.checkForm()
+      }
+      else{
+        fetch("http://localhost:3000/command/"+this.props.finalTranscript.toLowerCase()).then(data=>{
+          this.props.resetTranscript()
+          data.json().then(jsonData=>{
+            window.location.assign(jsonData[0].value)
+          })
+            .catch(error=>{
+              this.props.toggleMicrophone()
+              this.props.setMessage("Command not found.")
+              setTimeout(function(){
+                this.props.setMessage("")
+              }.bind(this), 3000)
+            })
+          
+        }).catch(error=>{
+          console.log(error.stack)
+          this.props.resetTranscript()
+        })
+      }
     }
-
+    componentDidUpdate(){
+      if(this.props.interimTranscript==='' && this.props.finalTranscript!==''){ 
+        this.testTranscript()
+      }
+    }
   render() {
-    const { transcript, browserSupportsSpeechRecognition, finalTranscript, interimTranscript} = this.props
+    const { browserSupportsSpeechRecognition, interimTranscript} = this.props
 
     if (!browserSupportsSpeechRecognition) {
       return null
     }
     return (
       <div className="transcriptContainer">
-        <div className="transcript">
-          {interimTranscript}
-        </div>
-        {interimTranscript==='' && finalTranscript!=='' ? this.testTranscript(): null}
+        <div className="transcript">{interimTranscript}</div>
+        
       </div>
     )
   }
